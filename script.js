4b76d6fffa9eca842048adf1cd313d297bbc45a9
terryproctor <terryproctor2015@outlook.com>
2021-02-18 23:51:22 +0000

    adding/removing divs

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ function changeToRed() {
     this.style.backgroundColor = 'red'
 }
 
+var editable = (document.getElementsByClassName('menuItem'));
+var lastEditable = (editable[editable.length-1]);
+lastEditable.firstChild.textContent = 'Chocolate';
+
+var secondEditable = (editable[1]);
+secondEditable.remove();
+
+newEl = document.createElement('div');
+newEl.className = ('menuItem');
+newText = document.createTextNode('Test');
+newEl.appendChild(newText);
+let position = document.getElementById('wrapper')
+position.appendChild(newEl);
+newEl.style.display = 'none';
